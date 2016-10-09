789a9c31a5bb675d4c7dddc7f93d60873ff533e7
Joann <joann.mondresku@gmail.com>
2016-10-09 13:02:41 +0300

    Changed Date.now() to use the message timestamps

--- runtime/commands/default.js
+++ runtime/commands/default.js
@@ -11,9 +11,9 @@ Commands.ping = {
   timeout: 10,
   level: 0,
   fn: function (msg) {
-    var initTime = Date.now()
+    var initTime = new Date(msg.timestamp)
     msg.reply('Pong!').then((m) => {
-      m.edit('<@' + msg.author.id + '>, Pong! Time taken ' + Math.floor(Date.now() - initTime) + ' ms.')
+      m.edit('<@' + msg.author.id + '>, Pong! Time taken ' + Math.floor(new Date(m.timestamp) - initTime) + ' ms.')
     })
   }
 }
