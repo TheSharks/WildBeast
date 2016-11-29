f67bcebd60cd5763f3a0dbdcbd72bc186f905111
Anthony Z <tony@lemonmc.com>
2016-11-29 17:01:16 -0500

    @Nenkai pls

--- runtime/commandcontrol.js
+++ runtime/commandcontrol.js
@@ -1,7 +1,7 @@
 'use strict'
 var directory = require('require-directory')
 var bugsnag = require('bugsnag')
-var config = require('../../config.json')
+var config = require('../config.json')
 bugsnag.register(config.api_keys.bugsnag)
 var com = directory(module, './commands', {
   exclude: /custom/
