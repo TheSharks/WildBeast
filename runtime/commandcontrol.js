c13c48471bb1314a53626bc7e7ef73215b60621e
Remco <hello@dougley.com>
2017-05-11 22:52:49 +0200

    Unfinished POC for the new command handler

--- runtime/commandcontrol.js
+++ runtime/commandcontrol.js
@@ -45,89 +45,43 @@ if (cus !== null) {
 }
 
 exports.helpHandle = function (msg, suffix) {
-  var msgArray = []
-  var msgArraytwo = []
-  var cmdone = []
+  var sorts = {
+    0: [
+      '**Available commands:**\n'
+    ]
+  }
+  let counter = 0
   if (!suffix) {
     for (var index in commands) {
-      if (commands[index].hidden || commands[index].level === 'master') {
-        continue
-      } else {
-        cmdone.push(commands[index].name + ' = "' + commands[index].help + '"')
+      if (!commands[index].hidden && commands[index].level !== 'master') {
+        if (sorts[counter].join('\n').length > 1750) {
+          counter++
+          sorts[counter] = []
+        }
+        sorts[counter].push(index + ' = "' + commands[index].help + '"')
       }
     }
-    var cmdtwo = cmdone.splice(0, cmdone.length / 2)
-    msgArray.push('**Available commands:** \n')
-    msgArray.push('```ini')
-    msgArray.push(cmdone.sort().join('\n') + '\n')
-    msgArray.push('```')
-    msgArraytwo.push('```ini')
-    msgArraytwo.push(cmdtwo.sort().join('\n') + '\n')
-    msgArraytwo.push('```')
-    msgArraytwo.push('If you want more information on the commands, check the command reference at http://docs.thesharks.xyz/commands.')
-    msgArraytwo.push('For further questions, join our server: https://discord.gg/wildbot')
-    msgArraytwo.push('Like what we do? Consider supporting my developer at Patreon! <https://www.patreon.com/Dougley>')
     msg.author.openDM().then((y) => {
       if (!msg.isPrivate) {
         msg.channel.sendMessage('Help is underway ' + msg.author.mention + '!')
       }
-      y.sendMessage(msgArray.join('\n'))
-      y.sendMessage(msgArraytwo.join('\n'))
+      for (var r of sorts) {
+        y.sendMessage(`\`\`\`ini\n${r.join('\n')}\n\`\`\``)
+      }
     }).catch((e) => {
       Logger.error(e)
       msg.channel.sendMessage('Well, this is awkward, something went wrong while trying to PM you. Do you have them enabled on this server?')
     })
   } else if (suffix) {
     if (commands[suffix] || alias[suffix]) {
-      var comad
-      if (alias[suffix]) {
-        comad = alias[suffix]
-      } else {
-        comad = commands[suffix]
-      }
-      if (comad.level === 'master' && require('../config.json').permissions.master.indexOf(msg.author.id) === -1) {
-        msg.reply('this command is not for you to use, therefor I will not tell you how to use it.')
-      } else {
-        msgArray = []
-        msgArray.push('Command name `' + comad.name + '`')
-        msgArray.push('What this does: `' + comad.help + '`\n')
-        msgArray.push('Example:')
-        if (comad.hasOwnProperty('usage')) {
-          msgArray.push('```' + `${require('../config.json').settings.prefix}${comad.name} ${comad.usage}` + '```')
-        } else {
-          msgArray.push('```' + `${require('../config.json').settings.prefix}${comad.name}` + '```')
-        }
-        msgArray.push(`**Required access level**: ${comad.level}`)
-        if (comad.hasOwnProperty('aliases')) {
-          msgArray.push(`**Aliases for this command**: ${comad.aliases.join(', ')}.`)
-        }
-        if (comad.hasOwnProperty('hidden')) {
-          msgArray.push('*Shh, this is a secret command.*')
-        }
-        if (comad.hasOwnProperty('timeout')) {
-          msgArray.push(`*This command has a timeout of ${comad.timeout} seconds.*`)
-        }
-        if (comad.hasOwnProperty('nsfw')) {
-          msgArray.push('*This command is NSFW.*')
-        }
-        if (comad.hasOwnProperty('noDM')) {
-          msgArray.push('*This command cannot be used in DM.*')
-        }
-        if (comad.name === 'meme') {
-          var str = '**Currently available memes:\n**'
-          var meme = require('./commands/memes.json')
-          for (var m in meme) {
-            str += m + ', '
+      var attributes = []
+      var c = (commands[suffix]) ? commands[suffix] : alias[suffix]
+      for (var attribute of c) {
+        switch (attribute) {
+          case 'level': {
+            // yadda yadda you get the point
           }
-          msgArray.push(str.substring(0, str.length - 2))
         }
-        msg.author.openDM().then((y) => {
-          y.sendMessage(msgArray.join('\n'))
-        }).catch((e) => {
-          Logger.error(e)
-          bugsnag.notify(e)
-          msg.channel.sendMessage('Whoops, try again.')
-        })
       }
     } else {
       msg.channel.sendMessage(`There is no **${suffix}** command!`)
