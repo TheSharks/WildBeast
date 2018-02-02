eb4adf454b2ca24c388351b2fbb08b165f7f460b
Remco <hello@dougley.com>
2018-02-02 11:05:53 +0100

    Timeouts and noDM support

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -2,7 +2,8 @@ const commands = require('../internal/command-indexer').commands
 const aliases = require('../internal/command-indexer').alias
 const engines = {
   perms: require('../engines/permissions'),
-  settings: require('../engines/settings')
+  settings: require('../engines/settings'),
+  timeout: require('../engines/timeouts')
 }
 const masters = process.env['WILDBEAST_MASTERS'].split('|')
 
@@ -16,7 +17,15 @@ module.exports = async (ctx) => {
     const suffix = msg.content.substr(prefix.length).split(' ').slice(1).join(' ')
     if (commands[cmd]) {
       if (commands[cmd].meta.level === Infinity && !masters.includes(msg.author.id)) {
-        return msg.channel.createMessage('This command is only for the bot owner')
+        return msg.channel.createMessage('This command is only for the bot owner.')
+      }
+      if (!msg.channel.guild && commands[cmd].meta.noDM) {
+        return msg.channel.createMessage('This command cannot be used in Direct Messages.')
+      }
+      let time = true
+      if (commands[cmd].meta.timeout) time = engines.timeout.calculate((msg.channel.guild ? msg.channel.guild.id : msg.author.id), cmd, commands[cmd].meta.timeout)
+      if (time !== true) {
+        return msg.channel.createMessage(`This command is on cooldown for another ${Math.floor(time)} seconds.`)
       }
       const res = (msg.channel.guild) ? await engines.perms.calculate(msg.channel.guild, msg.member) : await engines.perms.calculate(false, msg.author)
       if (res >= commands[cmd].meta.level) {
--- src/internal/database-selector.js
+++ src/internal/database-selector.js
@@ -3,8 +3,7 @@ const preferred = process.env['WILDBEAST_PREFERRED_DATABASE'] || 'arangodb'
 const available = Object.getOwnPropertyNames(drivers)
 
 if (available.indexOf(preferred) === -1) {
-  console.error(`FATAL: No database driver available called ${preferred}.\nAvailable choices: ${available.join(', ')}`)
-  process.exit(1)
+  global.logger.error(`No database driver available called ${preferred}, available choices: ${available.join(', ')}`, true)
 }
 
 module.exports = drivers[preferred]
