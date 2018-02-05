4278dab64c30df75d52829857830f2a7ac1d0205
Remco <hello@dougley.com>
2018-02-05 14:46:46 +0100

    style

--- src/internal/command-indexer.js
+++ src/internal/command-indexer.js
@@ -12,10 +12,12 @@ const aliases = new Map()
 for (const cmd in commands) {
   if (result[cmd]) global.logger.error(`Unable to register command ${cmd}, a command with this name already exists.`, true)
   result[cmd] = commands[cmd]
-  if (commands[cmd].meta.alias) for (const x of commands[cmd].meta.alias) {
-    if (commands[x]) global.logger.error(`Cannot set alias ${x}, there's a command with this name.`, true)
-    if (aliases.has(x)) global.logger.error(`Cannot set ${x} as an alias of ${cmd}, it's already in use by ${aliases.get(x)}.`, true)
-    aliases.set(x, cmd)
+  if (commands[cmd].meta.alias) {
+    for (const x of commands[cmd].meta.alias) {
+      if (commands[x]) global.logger.error(`Cannot set alias ${x}, there's a command with this name.`, true)
+      if (aliases.has(x)) global.logger.error(`Cannot set ${x} as an alias of ${cmd}, it's already in use by ${aliases.get(x)}.`, true)
+      aliases.set(x, cmd)
+    }
   }
 }
 
