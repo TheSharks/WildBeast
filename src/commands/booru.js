4de4d9aa67e45d2a3af0e3ac2ddf4fda40e1eefe
Remco <hello@dougley.com>
2018-02-24 16:44:25 +0100

    booru command

---
+++ src/commands/booru.js
@@ -0,0 +1,74 @@
+const SA = require('superagent')
+
+const sites = {
+  gelbooru: {
+    apiStyle: 'gelbooru',
+    baseURL: 'https://gelbooru.com/index.php'
+  },
+  rule34: {
+    apiStyle: 'gelbooru',
+    baseURL: 'https://rule34.xxx/index.php'
+  },
+  e621: {
+    apiStyle: 'e621',
+    baseURL: 'https://e621.net/post/index.json'
+  }
+}
+
+module.exports = {
+  meta: {
+    help: 'Query various booru sites for images.',
+    nsfw: true,
+    timeout: 1, // somewhat of an external ratelimit compliance
+    level: 0
+  },
+  fn: function (msg, suffix) {
+    const parts = suffix.split(' ')
+    if (!sites[parts[0]]) {
+      return global.i18n.send('BOORU_SITE_UNKNOWN', msg.channel, {
+        site: parts[0]
+      })
+    }
+    msg.channel.sendTyping()
+    switch (sites[parts[0]].apiStyle) {
+      case 'gelbooru': { // Gelbooru interpretation has xml conversion to provide full support, although we do prefer JSON
+        SA(sites[parts[0]].baseURL)
+          .query({page: 'dapi', s: 'post', q: 'index', tags: parts.slice(1).join(' ')})
+          .set({'User-Agent': 'Superagent Node.js'})
+          .then(res => {
+            const result = require('xml-js').xml2js(res.text, {compact: true})
+            if (result.posts.post.length < 1) {
+              return global.i18n.send('BOORU_NO_RESULTS', msg.channel, {
+                query: parts.slice(1).join(' ')
+              })
+            }
+            const count = Math.floor((Math.random() * result.posts.post.length))
+            global.i18n.send('BOORU_SUCCESS', msg.channel, {
+              query: parts.slice(1).join(' '),
+              url: result.posts.post[count]._attributes.file_url
+            })
+          })
+        break
+      }
+      case 'e621': {
+        // wow look at e621 being all special with their own api style
+        SA(sites[parts[0]].baseURL)
+          .query({limit: '50', tags: parts.slice(1).join(' ')})
+          .set({'Accept': 'application/json', 'User-Agent': 'Superagent Node.js'})
+          .then(res => {
+            if (res.body.length < 1) {
+              return global.i18n.send('BOORU_NO_RESULTS', msg.channel, {
+                query: parts.slice(1).join(' ')
+              })
+            }
+            const count = Math.floor((Math.random() * res.body.length))
+            global.i18n.send('BOORU_SUCCESS', msg.channel, {
+              query: parts.slice(1).join(' '),
+              url: res.body[count].file_url
+            })
+          })
+        break
+      }
+    }
+  }
+}
