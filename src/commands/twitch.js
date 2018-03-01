5a6bdffd00bbd0f20d408c017b602eb12d656e53
Remco <hello@dougley.com>
2018-03-01 21:00:39 +0100

    fix trashcode @caf203 TRASHCODE

--- src/commands/twitch.js
+++ src/commands/twitch.js
@@ -11,13 +11,13 @@ module.exports = {
     if (!suffix) {
       msg.channel.createMessage('No channel specified!')
     } else {
-      SA.get(`https:/api.twitch.tv/kraken/streams/${suffix}`)
+      SA.get(`https://api.twitch.tv/kraken/streams/${suffix}`)
         .set({'Accept': 'application/vnd.twitchtv.v3+json', 'Client-ID': process.env.TWITCH_ID})
         .end((err, response) => {
-          if (err && response.statusCode !== 200) global.logger.error(err)
+          if (err) global.logger.error(err)
           else if (!response.body) msg.channel.createMessage('The API returned an unconventional response.')
-          else if (response.stream !== null) msg.channel.createMessage(`**${suffix}** is currently live at <https://twitch.tv/${suffix}>.`)
-          else if (response.stream === null) msg.channel.createMessage(`**${suffix}** is not currently streaming.`)
+          else if (response.body.stream !== null) msg.channel.createMessage(`**${suffix}** is currently live at <https://twitch.tv/${suffix}>.`)
+          else if (response.body.stream === null) msg.channel.createMessage(`**${suffix}** is not currently streaming.`)
           else if (!err && response.statusCode === 404) msg.channel.createMessage(`**${suffix}** isn't a valid channel.`)
         })
     }
