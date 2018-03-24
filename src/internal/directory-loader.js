dfe5c0e0990b81721519376aa7ccfcd008dc4ef8
DrunkenPoney <drunkenponey@gmail.com>
2018-03-24 16:15:51 -0400

    Removed JSDoc

--- src/internal/directory-loader.js
+++ src/internal/directory-loader.js
@@ -1,18 +1,11 @@
 const path = require('path')
 
-/**
- * Imports a whole directory.
- *
- * @param location {string}
- * @param opts {{regex: RegExp, relative: boolean}}
- * @returns {{}}
- */
 module.exports = function (location, opts = {}) {
   let {regex, relative} = Object.assign({regex: /\.js$/, relative: true}, opts)
   let result = {}
 
   if (relative) {
-    let currentPath = path.dirname(new Error().stack.split('\n')[2].replace(/[^(]+\((.+):\d+:\d+\).*/g, (m, g1) => g1))
+    const currentPath = path.dirname(new Error().stack.split('\n')[2].replace(/[^(]+\((.+):\d+:\d+\).*/g, (m, g1) => g1))
     location = path.resolve(currentPath, location)
   }
 
