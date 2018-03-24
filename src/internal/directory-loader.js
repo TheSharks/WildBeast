74ce8dea96d8c029f2621a2a743c4a595ea3a669
DrunkenPoney_Qc <drunkenponey@gmail.com>
2018-03-24 16:30:09 -0400

    relative directory-loader (#317)
    closes #310

--- src/internal/directory-loader.js
+++ src/internal/directory-loader.js
@@ -1,9 +1,18 @@
-module.exports = function (location, regex = /\.js$/) {
+const path = require('path')
+
+module.exports = function (location, opts = {}) {
+  let {regex, relative} = Object.assign({regex: /\.js$/, relative: true}, opts)
   let result = {}
+
+  if (relative) {
+    const currentPath = path.dirname(new Error().stack.split('\n')[2].replace(/[^(]+\((.+):\d+:\d+\).*/g, (m, g1) => g1))
+    location = path.resolve(currentPath, location)
+  }
+
   require('fs').readdirSync(location).forEach(function (file) {
-    if (file.match(regex) !== null) {
+    if (regex.test(file)) {
       let name = file.replace(regex, '')
-      result[name] = require(require('path').resolve(location, name))
+      result[name] = require(path.resolve(location, name))
     }
   })
   return result
