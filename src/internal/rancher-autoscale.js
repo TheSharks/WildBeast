fd0e4e55a877dd2a8d9a4678a4fa4f2b15352acf
Remco <hello@dougley.com>
2018-04-16 18:49:10 +0200

    detect changes in scaling

--- src/internal/rancher-autoscale.js
+++ src/internal/rancher-autoscale.js
@@ -6,3 +6,11 @@ module.exports = new Promise(async (resolve, reject) => {
   const mine = await SA.get('http://rancher-metadata/latest/self/container/service_index')
   return resolve({total: parseInt(scale.text), mine: parseInt(mine.text) - 1})
 })
+
+setInterval(async () => {
+  const scale = await SA.get('http://rancher-metadata/latest/self/service/scale')
+  if (parseInt(scale.text) !== global.bot.options.maxShards) {
+    global.logger.warn(`Scaling configuration changed, restarting.`)
+    process.exit(0) // we could reconfigure the client somehow, but exiting is faster lol
+  }
+}, 10000)
