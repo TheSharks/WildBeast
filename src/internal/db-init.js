d15835b021a1537784995bf5216a83045908d0c5
Remco <hello@dougley.com>
2018-05-21 12:59:46 +0200

    fully remove logger from dbinit

--- src/internal/db-init.js
+++ src/internal/db-init.js
@@ -8,23 +8,22 @@ const tables = [
 ]
 
 const driver = require('arangojs')(process.env['ARANGO_URI'] || 'http://localhost:8529')
-const logger = console // dropin change
-
 driver.useBasicAuth(process.env['ARANGO_USERNAME'], process.env['ARANGO_PASSWORD'])
 
 if (process.env.WILDBEAST_PREFERRED_DATABASE && process.env.WILDBEAST_PREFERRED_DATABASE !== 'arangodb') {
-  logger.error(`This script will only work on ArangoDB, not on your requested driver, ${process.env.WILDBEAST_PREFERRED_DATABASE}`, true)
+  console.error(`This script will only work on ArangoDB, not on your requested driver, ${process.env.WILDBEAST_PREFERRED_DATABASE}`)
+  process.exit(1)
 }
 
 driver.listDatabases().then(async databases => {
   if (databases.includes(process.env.ARANGO_DATABASE || 'wildbeast')) {
-    logger.log('Database exists, checking for collections.')
+    console.log('Database exists, checking for collections.')
     driver.useDatabase(process.env.ARANGO_DATABASE || 'wildbeast')
     return driver.listCollections()
   } else {
-    logger.log('Database does not exist, creating...')
+    console.log('Database does not exist, creating...')
     await driver.createDatabase(process.env.ARANGO_DATABASE || 'wildbeast')
-    logger.log('Database created, moving on to creating collections.')
+    console.log('Database created, moving on to creating collections.')
     driver.useDatabase(process.env.ARANGO_DATABASE || 'wildbeast')
     return driver.listCollections()
   }
@@ -32,11 +31,11 @@ driver.listDatabases().then(async databases => {
   let names = []
   collections.map(c => names.push(c.name))
   tables.map(x => {
-    if (names.includes(x)) logger.log(`Collection ${x} already exists.`)
+    if (names.includes(x)) console.log(`Collection ${x} already exists.`)
     else {
       const coll = driver.collection(x)
       coll.create().then(() => {
-        logger.log(`Collection ${x} has been created.`)
+        console.log(`Collection ${x} has been created.`)
       })
     }
   })
