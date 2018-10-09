ba986a8c02ea8f7b4bf22224ba7370d1ceb109e4
Remco <superschaap11@outlook.com>
2018-10-09 08:53:41 +0200

    fix jpeg command bug + version bump

--- src/commands/jpeg.js
+++ src/commands/jpeg.js
@@ -11,7 +11,7 @@ module.exports = {
     const url = require('url')
     if (!msg.attachments && !url.parse(suffix).host) return global.i18n.send('INVALID_COMMAND_SYNTAX', msg.channel)
     const jimp = require('jimp')
-    jimp.read(msg.attachments[0].url || suffix).then(async image => {
+    jimp.read(msg.attachments[0] ? msg.attachments[0].url : suffix).then(async image => {
       const buffer = await image.quality(1).getBufferAsync(jimp.MIME_JPEG)
       msg.channel.createMessage('', {
         file: buffer,
