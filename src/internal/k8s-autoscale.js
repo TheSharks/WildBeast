d0b9f1836732c7126ae495aafac068f044835154
Remco <hello@dougley.com>
2018-12-10 19:36:04 +0100

    parseInt

--- src/internal/k8s-autoscale.js
+++ src/internal/k8s-autoscale.js
@@ -7,5 +7,5 @@ const OS = require('os')
 module.exports = new Promise(async (resolve, reject) => {
   if (!process.env.K8S_AUTOSCALE) return resolve({ total: process.env.WILDBEAST_SHARDS_TOTAL || 1, mine: process.env.WILDBEAST_SHARDS_MINE || 0 })
   const hs = OS.hostname().match(/[\w]+-([\d]+)/)[1]
-  return resolve({ total: process.env.WILDBEAST_SHARDS_TOTAL, mine: hs })
+  return resolve({ total: process.env.WILDBEAST_SHARDS_TOTAL, mine: parseInt(hs) })
 })
