6553022061b3449d380db2282b09d14ffd15b529
Pierce <spammermuch@gmail.com>
2020-05-02 10:57:47 -0400

    linted & changed as requested

--- src/commands/fun/xkcd.js
+++ src/commands/fun/xkcd.js
@@ -2,28 +2,37 @@ const SA = require('superagent')
 const Command = require('../../classes/Command')
 
 module.exports = new Command(async (msg, suffix) => {
-  const res = await SA.get('http://xkcd.com/info.0.json')
-  if(res.error) {
-    msg.channel.createMessage(`<@${msg.author.id}>, Please try again later.`)
-    global.logger.error(`REST call failed: ${error}`)
-  } else {
-    if(suffix.toLowerCase() === 'current') {
-      return msg.channel.createMessage(`<@${msg.author.id}>, **Alternate text (shown on mouse over)**\n ${res.body.alt}\n\n${res.body.img}`)
+  try {
+    const res = await SA.get('http://xkcd.com/info.0.json')
+    if (suffix.toLowerCase() === 'current') {
+      return msg.channel.createMessage(getEmbed(res.body))
     }
     let num = parseInt(suffix)
-    if(isNaN(num)) {
+    if (isNaN(num)) {
       num = Math.floor(Math.random() * (res.body.num - 1)) + 1
-    } else if(num > res.body.num) {
-      return msg.channel.createMessage(`<@${msg.author.id}>, There are only ${res.body.num} xkcd comics!`)
+    } else if (num > res.body.num) {
+      return msg.channel.createMessage(`There are only ${res.body.num} xkcd comics!`)
     }
-    SA.get(`http://xkcd.com/${num}/info.0.json`)
-      .end((error, response) => {
-        if(error) {
-          global.logger.error(`REST call failed: ${error}`)
-          msg.channel.createMessage(`<@${msg.author.id}>, Please try again later.`)
-        } else {
-          msg.channel.createMessage(`<@${msg.author.id}>, **Alternate text (shown on mouse over)**\n ${response.body.alt}\n\n${response.body.img}`)
-        }
-      })
+    const numRes = await SA.get(`http://xkcd.com/${num}/info.0.json`)
+    msg.channel.createMessage(getEmbed(numRes.body))
+  } catch (error) {
+    logger.error('REST XKCD', error)
+    msg.channel.createMessage('We ran into an error making that request, sorry about that!')
   }
 })
+
+function getEmbed (body) {
+  return {
+    embed: {
+      url: `https://xkcd.com/${body.num}/`,
+      title: body.safe_title,
+      color: 0x202225,
+      image: {
+        url: body.img
+      },
+      footer: {
+        text: body.alt
+      }
+    }
+  }
+}
