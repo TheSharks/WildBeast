3b9cf2060470b6bcef46f24bc8541e2f19d7a892
Remco <hello@dougley.com>
2020-06-19 14:07:54 +0200

    tweak some services

--- src/components/services/bots-gg.js
+++ src/components/services/bots-gg.js
@@ -4,8 +4,9 @@ const client = require('../client')
 module.exports = {
   fn: async () => {
     logger.debug('SERVICE-BOTSGG', 'Updating stats')
-    await SA
+    const res = await SA
       .post(`https://discord.bots.gg/api/v1/bots/${client.user.id}/stats`)
+      .retry(3)
       .type('json')
       .send({
         guildCount: client.guilds.length,
@@ -16,7 +17,10 @@ module.exports = {
         Authorization: process.env.WILDBEAST_BOTS_GG_KEY,
         'User-Agent': `${client.user.username}-${client.user.discriminator}/${require('../../../package.json').verison} (Eris; +https://github.com/TheSharks/WildBeast) DBots/${client.user.id}`
       })
+    logger.trace('SERVICE-BOTSGG', res)
   },
-  interval: 5000,
+  interval: Math.floor(Math.random() * 1000) + 5000,
+  // the interval is randomized since we cant anticipate when other shards might start posting stats
+  // if all shards start posting stats at the same time, we get ratelimited and data gets lost
   runCheck: () => !!process.env.WILDBEAST_BOTS_GG_KEY
 }
--- src/components/services/top-gg.js
+++ src/components/services/top-gg.js
@@ -4,8 +4,9 @@ const client = require('../client')
 module.exports = {
   fn: async () => {
     logger.debug('SERVICE-TOPGG', 'Updating stats')
-    await SA
+    const res = await SA
       .post(`https://top.gg/api/bots/${client.user.id}/stats`)
+      .retry(3)
       .type('json')
       .send({
         server_count: client.guilds.length,
@@ -14,9 +15,12 @@ module.exports = {
       })
       .set({
         Authorization: process.env.WILDBEAST_TOP_GG_KEY,
-        'User-Agent': `${client.user.username}-${client.user.discriminator}/${require('../../../package.json').verison} (+https://github.com/TheSharks/WildBeast`
+        'User-Agent': `${client.user.username}-${client.user.discriminator}/${require('../../../package.json').verison} (+https://github.com/TheSharks/WildBeast)`
       })
+    logger.trace('SERVICE-TOPGG', res)
   },
-  interval: 5000,
+  interval: Math.floor(Math.random() * 1000) + 5000,
+  // the interval is randomized since we cant anticipate when other shards might start posting stats
+  // if all shards start posting stats at the same time, we get ratelimited and data gets lost
   runCheck: () => !!process.env.WILDBEAST_TOP_GG_KEY
 }
