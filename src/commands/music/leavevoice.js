abff06650a6cc0f03091ec0f0c3f3a565b31fe5e
Remco <hello@dougley.com>
2020-06-23 20:04:13 +0200

    change leave method for voice

--- src/commands/music/leavevoice.js
+++ src/commands/music/leavevoice.js
@@ -4,7 +4,7 @@ module.exports = new Command(async msg => {
   const client = require('../../components/client')
   const encoder = client.voiceConnectionManager.get(msg.channel.guild.id)
   if (!encoder) return msg.channel.createMessage("I'm currently not streaming in this server")
-  client.voiceConnections.leave(msg.channel.guild)
+  encoder.destroy()
   client.voiceConnectionManager.delete(msg.channel.guild.id)
 }, {
   aliases: ['leave-voice', 'stop'],
