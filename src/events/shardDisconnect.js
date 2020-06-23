64f445b32dd0a64c335ab5624c4af04bc30d25e8
Remco <hello@dougley.com>
2020-06-23 21:10:11 +0200

    remove shardDC event

--- src/events/shardDisconnect.js
+++
@@ -1,4 +0,0 @@
-module.exports = (ctx) => {
-  if (ctx[0] !== null && ctx[0].message === 'Authentication failed') logger.error('ERIS', 'Your token is incorrect, authentication failed', true)
-  else (ctx[0] === null) ? logger.warn('ERIS', 'Client disconnected!') : logger.warn('ERIS', ctx[0])
-}
