e066f43261c914f3a32a3d58a2dc20d4e4b8e3f4
Remco <hello@dougley.com>
2020-08-09 15:32:28 +0200

    change api provider for randomcat random.cat seems to be defunct

--- src/commands/fun/randomcat.js
+++ src/commands/fun/randomcat.js
@@ -3,16 +3,15 @@ const Command = require('../../classes/Command')
 module.exports = new Command(async function (msg) {
   const SA = require('superagent')
   try {
-    const res = await SA.get('https://aws.random.cat/meow')
     const fact = (await SA.get('https://catfact.ninja/fact')).body.fact
     await this.safeSendMessage(msg.channel, {
       embed: {
         description: fact,
         image: {
-          url: res.body.file
+          url: 'https://cataas.com/cat'
         },
         footer: {
-          text: 'random.cat'
+          text: 'cataas.com'
         }
       }
     })
