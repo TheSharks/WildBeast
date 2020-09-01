e01f8a4eda55da4f85081d763b54ecf8c9d1eb30
Remco <hello@dougley.com>
2020-09-01 14:02:33 +0200

    cleanup

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,11 +1,11 @@
 const Command = require('../../classes/Command')
 
 module.exports = new Command(async function (msg, suffix) {
-  const m = await this.safeSendMessage(msg.channel, 'Working on it...')
-  try {
-    const client = require('../../components/client')
-    const player = client.voiceConnectionManager.get(msg.channel.guild.id)
-    if (player) {
+  const client = require('../../components/client')
+  const player = client.voiceConnectionManager.get(msg.channel.guild.id)
+  if (player) {
+    const m = await this.safeSendMessage(msg.channel, 'Working on it...')
+    try {
       const x = await player.resolve(suffix)
       switch (x.loadType) {
         case 'PLAYLIST_LOADED': {
@@ -45,12 +45,12 @@ module.exports = new Command(async function (msg, suffix) {
         case 'NO_MATCHES': return m.edit('Nothing found with your search query')
         default: return m.edit('Something went wrong while adding this track, try again later')
       }
-    } else return this.safeSendMessage(msg.channel, "I'm not streaming in this server")
-  } catch (e) {
-    logger.error('CMD', e)
-    if (!(m instanceof require('eris').Message)) await this.safeSendMessage(msg.channel, 'Something went wrong, try again?')
-    else await m.edit('Something went wrong, try again?')
-  }
+    } catch (e) {
+      logger.error('CMD', e)
+      if (!(m instanceof require('eris').Message)) await this.safeSendMessage(msg.channel, 'Something went wrong, try again?')
+      else await m.edit('Something went wrong, try again?')
+    }
+  } else return this.safeSendMessage(msg.channel, "I'm not streaming in this server")
 }, {
   aliases: ['request'],
   prereqs: ['musicCommand'],
