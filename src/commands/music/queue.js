e8979dda59bbf644f0f78eab7432664169294999
Remco <hey@dougley.com>
2020-09-17 18:57:59 +0200

    start converting some structures

--- src/commands/music/queue.js
+++ src/commands/music/queue.js
@@ -3,7 +3,7 @@ const Command = require('../../classes/Command')
 module.exports = new Command(async function (msg) {
   const client = require('../../components/client')
   const player = client.voiceConnectionManager.get(msg.channel.guild.id)
-  if (!player) return this.safeSendMessage(msg.channel, "I'm not streaming in this server")
+  if (!player) return this.safeSendMessage(msg.channel, i18n.t('commands.common.notStreaming'))
   return this.safeSendMessage(msg.channel, createEmbed(player.playlist))
 }, {
   clientPerms: {
@@ -16,11 +16,11 @@ module.exports = new Command(async function (msg) {
 const createEmbed = (ctx) => {
   return {
     embed: {
-      title: 'Current playlist',
+      title: i18n.t('commands.queue.title'),
       description:
         (ctx.length > 0)
-          ? `There are ${ctx.length} songs queued${ctx.length > 10 ? ', showing the next 10' : ''}`
-          : `Currently nothing is queued, queue something with \`${process.env.BOT_PREFIX}play\`!`,
+          ? i18n.t('commands.queue.count', { songs: ctx.length })
+          : i18n.t('commands.queue.empty', { command: `${process.env.BOT_PREFIX}play` }),
       fields: ctx.slice(0, 10).map(x => {
         return {
           name: ctx.indexOf(x) + 1,
--- src/internal/i18n.js
+++ src/internal/i18n.js
@@ -7,13 +7,14 @@ const defaultlang = process.env.WILDBEAST_LANGUAGE || 'en-EN'
 logger.debug('I18N', `Loaded ${Object.keys(languages).length} language(s), using ${defaultlang} as default`)
 
 module.exports = {
-  t (key, ctx) {
+  t (key, ctx, lang) {
     try {
-      const msg = key.split('.').reduce((o, i) => o[i], languages[defaultlang])
+      const msg = key.split('.').reduce((o, i) => o[i], languages[lang] || languages[defaultlang])
       return new IntlMessageFormat(msg, defaultlang).format(ctx)
     } catch (e) {
       logger.error('I18N', e)
       return '[TRANSLATION FAILED]'
     }
-  }
+  },
+  _languages: languages
 }
