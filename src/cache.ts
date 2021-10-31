c0bf6f583c2da1d0c2ebb6e0a0d99e90f9f01fe0
Remco <hey@dougley.com>
2021-10-31 19:40:33 +0100

    jobs

--- src/cache.ts
+++ src/cache.ts
@@ -1,1 +1,4 @@
+import { Job } from './jobs/base'
+
 export const languages = new Map<string, Record<string, any>>()
+export const jobs = new Map<string, Job>()
---
+++ src/jobs/bots-gg.cluster.ts
@@ -0,0 +1,59 @@
+import { ScheduledJob } from './base'
+import cluster from '../structures/cluster'
+import { ClusterClient } from 'detritus-client'
+import { jobs } from '../cache'
+import fetch from 'node-fetch'
+import { debug } from '../utils/logger'
+
+// no import - messes with tsc (package.json is outside the source dir)
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('../../package.json')
+
+const jobName = 'bots-gg'
+
+interface EvalReturnType {
+  guilds: number
+  user: {
+    username: string
+    discriminator: string
+    id: string
+  }
+}
+
+const job = new ScheduledJob(jobName)
+  .setInterval(1000 * 60 * 60) // every hour
+  .setExec(async () => {
+    const data: EvalReturnType[] = (await cluster.broadcastEval(() => {
+      // in the context of broadcastEval, 'this' is the client
+      const client = this as unknown as ClusterClient
+      return {
+        guilds: client.shards.reduce((a: number, b) => a + b.guilds.size, 0),
+        user: {
+          username: client.shards.get(client.shards.size - 1)!.user!.username,
+          discriminator: client.shards.get(client.shards.size - 1)!.user!.discriminator,
+          id: client.shards.get(client.shards.size - 1)!.user!.id
+        }
+      }
+    }))
+    const { user } = data[0]
+    const guilds = data.reduce((a: number, b) => a + b.guilds, 0)
+    const userAgent = `${user.username}-${user.discriminator}/${version as string} (Detritus; +https://github.com/TheSharks/WildBeast) DBots/${user.id}`
+    await fetch(`https://discord.bots.gg/api/bots/${user.id}/stats`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: process.env.BOTS_GG_TOKEN!,
+        'User-Agent': userAgent
+      },
+      body: JSON.stringify({
+        guildCount: guilds
+      })
+    })
+  })
+
+if (process.env.BOTS_GG_TOKEN !== undefined) {
+  job.start()
+  jobs.set(jobName, job)
+} else {
+  debug('Bots.gg stats disabled, missing BOTS_GG_TOKEN', jobName)
+}
