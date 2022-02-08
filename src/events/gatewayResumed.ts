17752b875cf9522579b1ea325ed6301794e611b5
Remco <hey@dougley.com>
2022-02-08 19:06:56 +0100

    more elastic logging

--- src/events/gatewayResumed.ts
+++ src/events/gatewayResumed.ts
@@ -1,7 +1,15 @@
 import { ClientEvents } from 'detritus-client/lib/constants'
 import client from '../structures/client'
+import { add } from '../utils/elastic'
 import { info } from '../utils/logger'
 
 client.client.subscribe(ClientEvents.GATEWAY_RESUMED, async function (payload) {
   info('Gateway resumed', `Gateway - shard ${payload.shard.shardId}`)
+  add({
+    type: 'gateway',
+    event: 'resumed',
+    shard: payload.shard.shardId,
+    trace: payload.raw._trace,
+    session: payload.shard.gateway.sessionId
+  })
 })
--- src/jobs/elastic-flush.client.ts
+++ src/jobs/elastic-flush.client.ts
@@ -1,13 +1,14 @@
 import { ScheduledJob } from './base'
-import { flush } from '../utils/elastic'
+import { flush, drop } from '../utils/elastic'
+import { debug } from '../utils/logger'
 
-const job = new ScheduledJob('analytics-prune')
+export default new ScheduledJob('elastic-flush')
   // every hour
   .setInterval(60 * 60 * 1000)
   .setExec(async () => {
-    await flush()
+    if (process.env.ELASTIC_URL !== undefined) {
+      debug('No elastic url set, skipping elastic flush and resetting cache', 'Elastic')
+      drop()
+    } else await flush()
   })
-
-if (process.env.ELASTIC_URL !== undefined) job.start()
-
-export default job
+  .start()
