752da70b63c168eb82b88337e04aa7914b8949fb
Remco <hey@dougley.com>
2022-02-12 15:31:12 +0100

    change flush interval

--- src/jobs/elastic-flush.client.ts
+++ src/jobs/elastic-flush.client.ts
@@ -3,8 +3,8 @@ import { flush, drop } from '../utils/elastic'
 import { debug } from '../utils/logger'
 
 export default new ScheduledJob('elastic-flush')
-  // every hour
-  .setInterval(60 * 60 * 1000)
+  // every 15 minutes
+  .setInterval(15 * 60 * 1000)
   .setExec(async () => {
     if (process.env.ELASTIC_URL === undefined) {
       debug('No elastic url set, skipping elastic flush and resetting cache', 'Elastic')
